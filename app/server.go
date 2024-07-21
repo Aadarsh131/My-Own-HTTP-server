5a02acd33c21d3198b04fc4cba9ccedc941f6a87
Aadarsh <aadarshkumar131@gmail.com>
2024-07-21 13:48:55 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
+	//net.Listen reserves the specified PORT for us
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
+	// for { //used to keep server listening on the port infinitely
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	//Read and Write is a blocking connection, make sure to use it wisely
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//localhost:4221/abcde returns 404 NOT FOUND,
+	//localhost:4221/ returns 200 OK
+	if strings.HasPrefix(string(buf), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+	}
+	conn.Close()
+	// }
 }
